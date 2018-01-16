e2414b4f264e06e090cc8e94ff887020af2589fa
remuspopasepa <remus.popa@sepa.at>
2018-01-16 11:40:45 +0200

    Configs and translations ---http://jira2.sepa.at/browse/HA-29
    Configs and translations ---http://jira2.sepa.at/browse/HA-29

--- local/modules/HookCustomer/I18n/frontOffice/default/en_US.php
+++ local/modules/HookCustomer/I18n/frontOffice/default/en_US.php
@@ -3,11 +3,11 @@
 return array(
     'Email address' => 'Email address',
     'Forgot your Password?' => 'Forgot your Password?',
-    'Log In!' => 'Log In!',
+    'Log In!' => 'Einloggen!',
     'Log out!' => 'Log out',
     'My Account' => 'My HADI Konto',
     'Password' => 'Password',
-    'Register' => 'Register',
-    'Register!' => 'Register!',
+    'Register' => 'Registrieren',
+    'Register!' => 'Registrieren!',
     'Sign In' => 'Sign In',
 );
